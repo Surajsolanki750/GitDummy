e97322c49315280ea0692869730620030a18b236
Suraj Solanki <suraj.solanki.750@gmail.com>
2023-10-05 16:30:29 +0530

    Feature updated 3.0.1

--- src/com/git/dummy1/DummyProject.java
+++ src/com/git/dummy1/DummyProject.java
@@ -5,7 +5,7 @@ public class DummyProject {
 	public static void main(String[] args) {
 		System.out.println("GitDummy");
 		System.out.println("Version 2.0");
-		System.out.println("Version 3.0");
+		System.out.println("Version 3.0.1");
 	}
 
 }
