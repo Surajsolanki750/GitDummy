b6741eb5dc6e73a7e67c45a594afee0619bde9ec
Suraj Solanki <suraj.solanki.750@gmail.com>
2023-10-05 17:21:49 +0530

    solved merge conflict

--- src/com/git/dummy1/DummyProject.java
+++ src/com/git/dummy1/DummyProject.java
@@ -5,8 +5,6 @@ public class DummyProject {
 	public static void main(String[] args) {
 		System.out.println("GitDummy");
 		System.out.println("Version 2.0");
-		System.out.println("Version 3.0");
-		System.out.println("Version 4.0");
 		System.out.println("Version 3.0.1");
 		System.out.println("Version 4.0");
 		System.out.println("Version 5.0");
